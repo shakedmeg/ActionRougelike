d5c93c3305e59a44e4d37818f01d36ee48c9e34d
shakedmeg <50861673+shakedmeg@users.noreply.github.com>
2026-03-25 15:10:42 +0200

    5.3 Interactable Item Chest

--- Source/ActionRougelike/World/RougeItemChest.cpp
+++ Source/ActionRougelike/World/RougeItemChest.cpp
@@ -6,9 +6,30 @@
 ARougeItemChest::ARougeItemChest()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	
+	BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComp"));
+	RootComponent = BaseMeshComponent;
+	
+	LidMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LidMeshComp"));
+	LidMeshComponent->SetupAttachment(BaseMeshComponent);
+}
+
+void ARougeItemChest::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorTickEnabled(true);
 }
 
 void ARougeItemChest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	CurrentAnimationPitch = FMath::FInterpConstantTo(CurrentAnimationPitch, AnimationTargetPitch, DeltaTime, AnimationSpeed);
+	LidMeshComponent->SetRelativeRotation(FRotator(CurrentAnimationPitch, 0.0f, 0.0f));
+	
+	if (FMath::IsNearlyEqual(CurrentAnimationPitch, AnimationTargetPitch))
+	{
+		SetActorTickEnabled(false);
+	}
 }
--- Source/ActionRougelike/World/RougeItemChest.h
+++ Source/ActionRougelike/World/RougeItemChest.h
@@ -11,6 +11,24 @@ class ACTIONROUGELIKE_API ARougeItemChest : public AActor
 {
 	GENERATED_BODY()
 
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> BaseMeshComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> LidMeshComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	float AnimationSpeed = 50.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	float AnimationTargetPitch = 120.f;
+	
+	float CurrentAnimationPitch = 0.0f;
+	
+	virtual void BeginPlay() override;
+
 public:
 	
 	ARougeItemChest();
