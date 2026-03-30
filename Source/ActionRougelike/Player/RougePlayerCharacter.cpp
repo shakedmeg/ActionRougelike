752d11585362a131764b28d70dd421a331a6327e
shakedmeg <50861673+shakedmeg@users.noreply.github.com>
2026-03-30 12:30:49 +0300

    9.4 Adjusting Projectile Direction

--- Source/ActionRougelike/Player/RougePlayerCharacter.cpp
+++ Source/ActionRougelike/Player/RougePlayerCharacter.cpp
@@ -4,12 +4,18 @@
 #include "RougePlayerCharacter.h"
 #include "EnhancedInputComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include "RougeGameTypes.h"
 #include "ActionSystem/RougeActionSystemComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/PawnMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "Projectiles/RougeProjectileMagic.h"
+
+
+TAutoConsoleVariable<float> CVarProjectileAdjustmentDebugDraw(TEXT("game.projectile.DebugDraw"), 0.0f,
+	TEXT("Enable projectile aim adjustment debug rendering. (0 = off, > 0 is duration)"),
+	ECVF_Cheat);
 
 
 // Sets default values
@@ -107,14 +113,54 @@ void ARougePlayerCharacter::StartProjectileAttack(TSubclassOf<ARougeProjectile> 
 void ARougePlayerCharacter::ProjectileTimerElapsed(TSubclassOf<ARougeProjectile> ProjectileClass)
 {
 	FVector SpawnLocation = GetMesh()->GetSocketLocation(MuzzleSocketName);
-	FRotator SpawnRotation = GetControlRotation();
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.Instigator = this;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 	
-	AActor* NewProjectile = GetWorld()->SpawnActor<ARougeProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, SpawnParams);
+	FVector EyeLocation = CameraComponent->GetComponentLocation();
+	FRotator EyeRotation = GetControlRotation();
+	
+	FVector TraceEnd = EyeLocation + (EyeRotation.Vector() * 5000.f);
+	
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	
+	UWorld* World = GetWorld();
+	
+	FVector AdjustedTargetLocation;
+	FHitResult Hit;
+	if (World->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, COLLISION_PROJECTILE, QueryParams))
+	{
+		AdjustedTargetLocation = Hit.Location;
+	}
+	else
+	{
+		AdjustedTargetLocation = TraceEnd;
+	}
+	
+	FRotator SpawnRotation = (AdjustedTargetLocation - SpawnLocation).Rotation();;
+	
+	AActor* NewProjectile = World->SpawnActor<ARougeProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, SpawnParams);
 	
 	MoveIgnoreActorAdd(NewProjectile);
+
+#if !UE_BUILD_SHIPPING	
+	float DebugDrawDuration = CVarProjectileAdjustmentDebugDraw.GetValueOnGameThread();
+	if (DebugDrawDuration > 0.0f)
+	{
+		// The hit location or trace end
+		DrawDebugBox(World, AdjustedTargetLocation, FVector(20.f), FColor::Green, false, DebugDrawDuration);
+		
+		// Adjustment line trace
+		DrawDebugLine(World, EyeLocation, TraceEnd, FColor::Green, false, DebugDrawDuration);
+		
+		// New projectile path
+		DrawDebugLine(World, SpawnLocation, AdjustedTargetLocation, FColor::Yellow, false, DebugDrawDuration);
+		
+		// The original path of the projectile
+		DrawDebugLine(World, SpawnLocation, SpawnLocation + GetControlRotation().Vector() * 5000.0f, FColor::Purple, false, DebugDrawDuration);
+	}
+#endif
 }
 
 void ARougePlayerCharacter::OnHealthChanged(float NewHealth, float OldHealth)
--- Source/ActionRougelike/RougeGameTypes.h
+++ Source/ActionRougelike/RougeGameTypes.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #define COLLISION_INTERACTION ECC_GameTraceChannel1
+#define COLLISION_PROJECTILE ECC_GameTraceChannel2
